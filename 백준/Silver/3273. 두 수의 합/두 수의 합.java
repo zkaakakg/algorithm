562d5a303414e3ad4cb959a9615877d2f47b6213
zkaakakg <cscom222@naver.com>
2025-03-26 15:58:45 +0900

    [Silver III] Title: 두 수의 합, Time: 336 ms, Memory: 36300 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -12,20 +12,36 @@ public class Main {
     }
     
     int x = Integer.parseInt(br.readLine());
+    Map<Integer, Integer> map = new HashMap<>();
+
+
     int cnt = 0;
-    int left = 0; int right = n-1;
+    // int left = 0; int right = n-1;
     
-    Arrays.sort(arr);
-    while (left<right){
-      int sum = arr[left] + arr[right];
-      if(sum == x){
+    // Arrays.sort(arr);
+    // while (left<right){
+    //   int sum = arr[left] + arr[right];
+    //   if(sum == x){
+    //     cnt++;
+    //     right--;
+    //     left++;
+    //   }else if(sum < x){
+    //     left++;
+    //   }else{
+    //     right--;
+    //   }
+    // }
+
+    for (int num : arr) {
+      int complement = x - num; // 필요한 짝 수
+      
+      // 필요한 수가 이미 맵에 있으면 카운트 증가
+      if (map.getOrDefault(complement, 0) > 0) {
         cnt++;
-        right--;
-        left++;
-      }else if(sum < x){
-        left++;
-      }else{
-        right--;
+        map.put(complement, map.get(complement) - 1); // 사용된 숫자는 줄이기
+      } else {
+        // 현재 숫자를 맵에 저장
+        map.put(num, map.getOrDefault(num, 0) + 1);
       }
     }
 
