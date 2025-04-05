9b92e95994bd7f5d1337230fee5c8a8a1e23728c
zkaakakg <cscom222@naver.com>
2025-04-05 16:40:12 +0900

    [Gold IV] Title: DSLR, Time: 4196 ms, Memory: 303936 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.java
+++ 백준/Gold/9019. DSLR/DSLR.java
@@ -4,7 +4,6 @@ import java.util.*;
 public class Main {
     static int a, b;
     static List<String> result;
-    static String[] command = {"D", "S", "L", "R"};
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -29,37 +28,37 @@ public class Main {
         boolean[] visited = new boolean[10000];
         Queue<Node> queue = new LinkedList<>();
 
-        queue.offer(new Node(a, ""));
+        queue.add(new Node(a, ""));
         visited[a] = true;
 
         while (!queue.isEmpty()) {
             Node current = queue.poll();
 
+            int d = (current.num * 2) % 10000;
+            int s = (current.num == 0) ? 9999 : current.num - 1;
+            int l = (current.num % 1000) * 10 + (current.num / 1000);
+            int r = (current.num % 10) * 1000 + (current.num / 10);
+
             if (current.num == b) {
                 result.add(current.commands);
-                return;
+                break;
             }
-            int d = (current.num * 2) % 10000;
+
             if (!visited[d]) {
                 visited[d] = true;
-                queue.offer(new Node(d, current.commands + "D"));
+                queue.add(new Node(d, current.commands + "D"));
             }
-            int s = (current.num == 0) ? 9999 : current.num - 1;
             if (!visited[s]) {
                 visited[s] = true;
-                queue.offer(new Node(s, current.commands + "S"));
+                queue.add(new Node(s, current.commands + "S"));
             }
-
-            int l = (current.num % 1000) * 10 + (current.num / 1000);
             if (!visited[l]) {
                 visited[l] = true;
-                queue.offer(new Node(l, current.commands + "L"));
+                queue.add(new Node(l, current.commands + "L"));
             }
-
-            int r = (current.num % 10) * 1000 + (current.num / 10);
             if (!visited[r]) {
                 visited[r] = true;
-                queue.offer(new Node(r, current.commands + "R"));
+                queue.add(new Node(r, current.commands + "R"));
             }
         }
     }
