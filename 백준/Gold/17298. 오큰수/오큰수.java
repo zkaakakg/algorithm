8af4b3ab30c64923528981afd909bfe6e9ac29a2
zkaakakg <cscom222@naver.com>
2025-04-12 18:40:16 +0900

    [Gold IV] Title: 오큰수, Time: 892 ms, Memory: 166516 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        int[] result = new int[n];
+        String[] input = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(input[i]);
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!stack.empty() && arr[stack.peek()] < arr[i]) {
+                result[stack.pop()] = arr[i];
+            }
+            stack.push(i);
+        }
+        while (!stack.isEmpty()) {
+            result[stack.pop()] = -1;
+        }
+
+//        for (int i = 0; i < n; i++) {
+//            System.out.print(result[i] + " ");
+//        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(result[i]).append(" ");
+        }
+        System.out.println(sb.toString());
+    }
+}
