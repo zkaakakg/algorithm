9b3626e7dcef502e3ce46b62c5255354e4360bba
zkaakakg <cscom222@naver.com>
2025-04-14 20:14:19 +0900

    [Silver III] Title: 이친수, Time: 100 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -8,9 +8,8 @@ public class Main {
 
         long[] dp = new long[n+1];
         dp[1] = 1;
-        if (n >= 2) dp[2] = 1;
 
-        for (int i = 3; i <= n ; i++) {
+        for (int i = 2; i <= n ; i++) {
             dp[i] = dp[i-1] + dp[i-2];
         }
 
