4b83d83ab183f658288436fda9a0c210b578e21c
zkaakakg <cscom222@naver.com>
2025-05-06 21:34:50 +0900

    [Silver II] Title: 이상한 술집, Time: 156 ms, Memory: 17156 KB -BaekjoonHub

---
+++ 백준/Silver/13702. 이상한 술집/이상한 술집.java
@@ -0,0 +1,34 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int k = Integer.parseInt(input[1]);
+
+        int[] arr = new int[n];
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            max = Math.max(max, arr[i]);
+        }
+
+        long left = 1;
+        long right = max;
+        long result = 0;
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            long cnt = 0;
+            for (int i = 0; i < n; i++) {
+                cnt += arr[i] / mid;
+            }
+            if (cnt >= k) {
+                result = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        System.out.println(result);
+    }
+}
