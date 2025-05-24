9b9eb363af0d6f4dc5c948ab3130ba38245aff37
zkaakakg <cscom222@naver.com>
2025-05-24 23:24:26 +0900

    [Gold IV] Title: 리모컨, Time: 168 ms, Memory: 14692 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.java
+++ 백준/Gold/1107. 리모컨/리모컨.java
@@ -1,18 +1,34 @@
 import java.io.*;
 
 public class Main {
+    static int n, m;
+    static boolean[] arr;
+
+    public static int solution(int i){
+        if (i == 0){
+            return arr[0] ? 0 : 1;
+        }
+
+        int len = 0;
+        while (i > 0){
+            if (arr[i % 10]) return 0;
+            i /= 10;
+            len++;
+        }
+        return len;
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
 
         if (n == 100) {
             System.out.println(0);
             return;
         }
 
-        boolean[] arr = new boolean[10];
+        arr = new boolean[10];
         if ( m != 0){
             String[] input = br.readLine().split(" ");
             for (int i = 0; i < m; i++) {
@@ -22,17 +38,9 @@ public class Main {
 
         int result = Math.abs(n - 100);
         for (int i = 0; i < 1000000; i++) {
-            String num = String.valueOf(i);
-
-            boolean isCheck = false;
-            for (int j = 0; j < num.length(); j++) {
-                if (arr[num.charAt(j) - '0']){
-                    isCheck = true;
-                }
-            }
-
-            if (!isCheck) {
-                int min = Math.abs(n - i) + num.length();
+            int len = solution(i);
+            if (len > 0){
+                int min = len + Math.abs(n - i);
                 result = Math.min(min, result);
             }
         }
