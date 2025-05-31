f9f37d03836745ac0c93c32d01b12c0db5f7489f
zkaakakg <cscom222@naver.com>
2025-05-31 17:41:33 +0900

    [Gold V] Title: 나는 기말고사형 인간이야, Time: 668 ms, Memory: 168436 KB -BaekjoonHub

---
+++ 백준/Gold/23254. 나는 기말고사형 인간이야/나는 기말고사형 인간이야.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+
+        int n = Integer.parseInt(input[0]);
+        int m = Integer.parseInt(input[1]);
+        int[] a = new int[m];
+        int[] b = new int[m];
+
+        input = br.readLine().split(" ");
+        for (int i = 0; i < m; i++) {
+            a[i] = Integer.parseInt(input[i]);
+        }
+
+        input = br.readLine().split(" ");
+        for (int i = 0; i < m; i++) {
+            b[i] = Integer.parseInt(input[i]);
+        }
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o2[0], o1[0]));
+        int time = 24 * n;
+        int[] score = Arrays.copyOf(a, m);
+
+        for (int i = 0; i < m; i++) {
+            if (score[i] < 100) {
+                int maxTime = Math.min(b[i], 100 - score[i]);
+                pq.offer(new int[]{maxTime, i});
+            }
+        }
+
+        while (time > 0 && !pq.isEmpty()) {
+            int[] cur = pq.poll();
+            int i = cur[1];
+
+            if (score[i] >= 100) continue;
+
+            score[i] += cur[0];
+            if (score[i] > 100) score[i] = 100;
+
+            time--;
+
+            if (score[i] < 100) {
+                int t = Math.min(b[i], 100-score[i]);
+                pq.offer(new int[]{t, i});
+            }
+
+        }
+
+        int result = 0;
+        for (int s : score) result += s;
+        System.out.println(result);
+    }
+}
